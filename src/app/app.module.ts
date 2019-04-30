ab1e8a8cc01ed07c1299f773622da4d824676c78
admin <admin@almogtama3.com>
2019-04-30 23:48:45 +0300

    temporary remove EldeebModule & EldeebService
    eldeeb was imported from dist, i.e the library must be compiled before 
    running the CMS
    now it better to be imported from the source directly
    
    - EldeebService (index.ts) was Angular service (i.e Injectable())
    now it is a normal class, to be run in any invironment (apart from 
    Angular)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
+//todo: EldeebModule,EldeebService was impoeted from dist, import them from src i.e: projects/eldeeb/src/..
+
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ErrorComponent } from "./error/error.component";
-import { EldeebModule } from "eldeeb";
-import { EldeebService } from "eldeeb";
+//import { EldeebModule } from "eldeeb";
+//import { EldeebService } from "eldeeb"; //index.ts
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { GestureConfig } from "@angular/material";
 import { HAMMER_GESTURE_CONFIG } from "@angular/platform-browser";
@@ -13,18 +15,18 @@ import { HAMMER_GESTURE_CONFIG } from "@angular/platform-browser";
   imports: [
     BrowserModule.withServerTransition({ appId: "serverApp" }),
     AppRoutingModule,
-    EldeebModule,
+    //EldeebModule,
     BrowserAnimationsModule
   ],
   providers: [
-    EldeebService,
+    //  EldeebService, //eldeeb/index now is not a service
     { provide: HAMMER_GESTURE_CONFIG, useClass: GestureConfig }
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(public eldeebService: EldeebService) {
+  /*constructor(public eldeebService: EldeebService) {
     console.log("*** AppModule started ***");
     eldeebService.test();
-  }
+  }*/
 }
