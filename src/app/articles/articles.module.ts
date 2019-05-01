08988fcba8becf7074af2ac5f97ea71948599ec6
Xx159753@@ <xxyyzz2050@gmail.com>
2019-05-01 09:14:32 +0300

    remove GetArticlesService from providors
    - GetArticlesService now exports a function (in addition to the service 
    itself), in most cases we can directly use the function, unless we need 
    to use the class itself, so we don't need to inject it and no need to 
    add it to providers

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -5,11 +5,10 @@ import { ArticlesRoutingModule } from "./articles-routing.module";
 import { ArticleComponent } from "./article/article.component";
 import { IndexComponent } from "./index/index.component";
 import { ManageComponent } from "./manage/manage.component";
-import { GetArticlesService } from "./get-articles.service";
 
 @NgModule({
   declarations: [ArticleComponent, IndexComponent, ManageComponent],
   imports: [CommonModule, ArticlesRoutingModule],
-  providers: [GetArticlesService]
+  providers: []
 })
 export class ArticlesModule {}
